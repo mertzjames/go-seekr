e9a2ff33d010b66e83076db3ee4c3030d94e2adf
James Mertz <2474567+mertzjames@users.noreply.github.com>
2025-08-18 18:48:22 -0700

    added some tests for the unit testable functions

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
+	"slices"
 	"testing"
 )
 
-func TestCheckIfText(t *testing.T) {
+func Test_checkIfText(t *testing.T) {
 	tests := []struct {
 		name     string
 		filePath string
@@ -22,6 +23,72 @@ func TestCheckIfText(t *testing.T) {
 			if got != tt.expected {
 				t.Errorf("checkIfText(%q) = %v; want %v", tt.filePath, got, tt.expected)
 			}
+		})
+	}
+}
+
+func Test_uniqueStr(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []string
+		expected []string
+	}{
+		{"No duplicates", []string{"a", "b", "c"}, []string{"a", "b", "c"}},
+		{"With duplicates", []string{"a", "b", "a", "c"}, []string{"a", "b", "c"}},
+		{"All duplicates", []string{"a", "a", "a"}, []string{"a"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := unique(tt.input)
+			if !slices.Equal(got, tt.expected) {
+				t.Errorf("unique(%v) = %v; want %v", tt.input, got, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_uniqueInt(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{"No duplicates", []int{1, 2, 3}, []int{1, 2, 3}},
+		{"With duplicates", []int{1, 2, 1, 3}, []int{1, 2, 3}},
+		{"All duplicates", []int{1, 1, 1}, []int{1}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := unique(tt.input)
+			if !slices.Equal(got, tt.expected) {
+				t.Errorf("uniqueInt(%v) = %v; want %v", tt.input, got, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_uniqueSlices(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    [][]int
+		expected [][]int
+	}{
+		{"No duplicates", [][]int{{1, 2}, {3, 4}}, [][]int{{1, 2}, {3, 4}}},
+		{"With duplicates", [][]int{{1, 2}, {2, 3}, {1, 2}}, [][]int{{1, 2}, {2, 3}}},
+		{"All duplicates", [][]int{{1, 2}, {1, 2}, {1, 2}}, [][]int{{1, 2}}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := uniqueSlices(tt.input)
+			for i := range got {
+				if !slices.Equal(got[i], tt.expected[i]) {
+					t.Errorf("uniqueSlices(%v) = %v; want %v", tt.input, got, tt.expected)
+				}
+			}
+
 		})
 	}
 }
